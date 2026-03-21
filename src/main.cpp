078305db59fb17472a1544f038c1dd7f6628debd
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-03-21 02:51:57 +0000

    platform background

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,21 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+#include <bn_regular_bg_ptr.h>
+
+#include "bn_regular_bg_items_platforms.h"
+#include "bn_sprite_items_robot.h"
+
 
 int main() {
     bn::core::init();
 
-    bn::backdrop::set_color(bn::color(31, 0, 0));
+    auto bg = bn::regular_bg_items::platforms.create_bg();
+
+    auto robot = bn::sprite_items::robot.create_sprite();
 
     while(true) {
-        if(bn::keypad::a_pressed()) {
-            bn::backdrop::set_color(bn::color(0, 0, 30));
-        }
-        if(bn::keypad::b_pressed()) {
-            bn::backdrop::set_color(bn::color(0, 31, 0));
-        }
         bn::core::update();
     }
 }
