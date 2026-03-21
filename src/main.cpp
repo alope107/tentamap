a2ca2d74bc34b951817db66bca06efda5911edae
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-03-21 03:03:29 +0000

    schmoovin

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@
 #include "bn_regular_bg_items_platforms.h"
 #include "bn_sprite_items_robot.h"
 
+inline bn::fixed tribool(bool p, bool q, bn::fixed magnitude) {
+    return p ? magnitude : q ? -magnitude : 0;
+}
+
+inline bn::fixed moveTribool(bn::keypad::key_type p, bn::keypad::key_type q, bn::fixed magnitude) {
+    return tribool(bn::keypad::held(p), bn::keypad::held(q), magnitude);
+}
+
 
 int main() {
     bn::core::init();
@@ -16,7 +24,16 @@ int main() {
 
     auto robot = bn::sprite_items::robot.create_sprite();
 
+    bn::fixed speed = 2;
+
     while(true) {
+        bn::fixed_point delta = { 
+            moveTribool(bn::keypad::key_type::RIGHT, bn::keypad::key_type::LEFT, speed),
+            moveTribool(bn::keypad::key_type::DOWN, bn::keypad::key_type::UP, speed)
+        };
+
+        robot.set_position(robot.position() + delta);
+
         bn::core::update();
     }
 }
