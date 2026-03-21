549beb016ae55e3b9719b0c6c7cc7b8ba1829c5f
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-03-21 04:01:28 +0000

    Jank collision

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <bn_keypad.h>
 #include <bn_sprite_ptr.h>
 #include <bn_regular_bg_ptr.h>
+#include <bn_regular_bg_map_cell_info.h>
+#include <bn_log.h>
 
 #include "bn_regular_bg_items_platforms.h"
 #include "bn_sprite_items_robot.h"
@@ -17,14 +19,19 @@ inline bn::fixed moveTribool(bn::keypad::key_type p, bn::keypad::key_type q, bn:
 }
 
 
+
+
 int main() {
     bn::core::init();
 
-    auto bg = bn::regular_bg_items::platforms.create_bg();
-
     auto robot = bn::sprite_items::robot.create_sprite();
 
-    bn::fixed speed = 2;
+    auto bg = bn::regular_bg_items::platforms.create_bg();
+    auto map = bn::regular_bg_items::platforms.map_item();
+    auto validCell = map.cell(0, 0);
+    int validTileIndex = bn::regular_bg_map_cell_info(validCell).tile_index();
+
+    bn::fixed speed = 1;
 
     while(true) {
         bn::fixed_point delta = { 
@@ -32,7 +39,22 @@ int main() {
             moveTribool(bn::keypad::key_type::DOWN, bn::keypad::key_type::UP, speed)
         };
 
-        robot.set_position(robot.position() + delta);
+        bn::fixed_point newPos = {robot.position() + delta};
+
+        //
+        bn::regular_bg_map_cell mapCell = map.cell({(newPos.x() / 8).round_integer() + ((256/2) / 8),
+                                                    (newPos.y() / 8).round_integer() + ((256/2) / 8)});
+
+        int cellIdx = bn::regular_bg_map_cell_info(mapCell).tile_index();
+
+
+        if(cellIdx != validTileIndex) {
+            BN_LOG("Boop");
+        } else {
+            BN_LOG("nah");
+            robot.set_position(newPos);
+        }
+        
 
         bn::core::update();
     }
